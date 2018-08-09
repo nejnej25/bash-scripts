d7b89456df2a9e07e2d5be859915f4967f030f5e
nejnej25 <perryjamesbelleza@gmail.com>
2018-08-09 22:21:47 +0800

    commit

---
+++ uuid-passwd-gen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Author: Perry
+
+if [[ $# -eq 1 && $1 =~ ^[0-9]+$ ]]; then
+	length=$1
+	generated=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $length | head -1)
+	echo "Generating steam of characters.."
+	sleep 2
+	echo "Generation done: $generated"
+else
+	echo "Usage: uuid-passwd-gen.sh <length>"
+fi
