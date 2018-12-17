c6af17acb4beebfa02ba3ba1096eac0718f5f67c
nejnej25 <perryjamesbelleza@gmail.com>
2018-12-17 07:27:03 +0000

    change sleep value

--- uuid-passwd-gen.sh
+++ uuid-passwd-gen.sh
@@ -5,7 +5,7 @@ if [[ $# -eq 1 && $1 =~ ^[0-9]+$ ]]; then
 	length=$1
 	generated=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $length | head -1)
 	echo "Generating steam of characters.."
-	sleep 2
+	sleep 1
 	echo "Generation done: $generated"
 else
 	echo "Usage: uuid-passwd-gen.sh <length>"
